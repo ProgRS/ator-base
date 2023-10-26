612f78da844490fe4f1562681dd8bb136b0bbed3
ProgRs <lfernandotc@hotmail.com>
2023-10-26 18:26:58 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+
+
+
+
+export default function App() {
+  return (
+    <ScrollView>
+    <View >
+      
+    </View>
+    </ScrollView>
+  );
+}
